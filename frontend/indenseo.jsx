84b5920baf965ec889e318485a79b5c2b88b8a17
Matt Allen <matt.allen1080@gmail.com>
2016-05-20 19:50:45 -0400

    get react working

---
+++ frontend/indenseo.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+
+  render: function () {
+    return (
+      <div className="yoyoyo">
+      </div>
+    );
+  }
+
+});
+
+$(
+  function () {
+    ReactDOM.render(<App />, document.getElementById("main"));
+  }
+);
